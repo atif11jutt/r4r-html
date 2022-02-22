4c206c6110b2abf4413eef0055bccd1d1420d523
atif11jutt <atif11jutt@gmail.com>
2022-02-23 02:45:18 +0500

    services page updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,157 +1,189 @@
 $(document).ready(function () {
+  // logo animation
+  let logo_diff = 140;
+  //
+  let diff = 35;
+  logo_diff += diff;
+  $(document).scroll(function () {
+    let scrollTop = $(document).scrollTop();
+    if (scrollTop >= 10 && scrollTop < logo_diff) {
+      let opacity = 5 / scrollTop;
+      $(".logo_delete").css("opacity", opacity);
+      if (scrollTop >= diff && scrollTop <= logo_diff) {
+        $(".logo_move").css(
+          "transform",
+          "translateX(-" + (scrollTop - diff) + "px)"
+        );
+      } else {
+        $(".logo_move").css("transform", "translateX(0px)");
+      }
+    } else if (scrollTop >= logo_diff) {
+      $(".logo_move").css(
+        "transform",
+        "translateX(-" + (logo_diff - diff) + "px)"
+      );
+      $(".logo_delete").css("opacity", "0");
+    } else {
+      $(".logo_move").css("transform", "translateX(0px)");
+      $(".logo_delete").css("opacity", "1");
+    }
 
+    if ($(window).width() <= 1080) {
+      if (scrollTop >= 200) {
+        $("header").css("transform", "translateY(-50vh)");
+      } else {
+        $("header").css("transform", "translateY(0vh)");
+      }
+    }
+  });
 
-    // logo animation
-    let logo_diff = 140;
-    // 
-    let diff = 35;
-    logo_diff += diff;
-    $(document).scroll(function () {
-        let scrollTop = $(document).scrollTop();
-        if (scrollTop >= 10 && scrollTop < logo_diff) {
-            let opacity = 5 / scrollTop;
-            $('.logo_delete').css("opacity", opacity);
-            if (scrollTop >= diff && scrollTop <= logo_diff) {
-                $(".logo_move").css("transform", "translateX(-" + (scrollTop - diff) + "px)")
-            } else {
-                $(".logo_move").css("transform", "translateX(0px)")
-            }
-        } else if (scrollTop >= logo_diff) {
-            $(".logo_move").css("transform", "translateX(-" + (logo_diff - diff) + "px)");
-            $('.logo_delete').css("opacity", "0");
-        } else {
-            $(".logo_move").css("transform", "translateX(0px)")
-            $('.logo_delete').css("opacity", "1");
-        }
+  // detect language
 
-        if ($(window).width() <= 1080) {
-            if (scrollTop >= 200) {
-                $("header").css("transform", "translateY(-50vh)")
-            } else {
-                $("header").css("transform", "translateY(0vh)")
-            }
-        }
+  let init_lang = localStorage.getItem("lang");
+  let default_lang = "en";
+
+  if (init_lang != null) {
+    $("#lang ." + init_lang)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+    changeLang(init_lang);
+  } else {
+    $("#lang ." + default_lang)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+    changeLang(default_lang);
+  }
+
+  // click event on language
+
+  $("#lang > div").click(function () {
+    if ($(this).hasClass("active")) {
+      $(this).parent().toggleClass("show");
+    } else {
+      $(this).addClass("active").siblings().removeClass("active");
+      $(this).parent().removeClass("show");
+      let lang = $(this).data("lang");
+      localStorage.setItem("lang", lang);
+      changeLang(lang);
+    }
+  });
+
+  // custom function
+  function changeLang(lang) {
+    $(".lang").each(function () {
+      $(this).text($(this).data(lang));
     });
 
-
-    // detect language
-
-    let init_lang = localStorage.getItem("lang");
-    let default_lang = "en";
-    
-    if(init_lang != null){
-        $("#lang ." + init_lang).addClass("active").siblings().removeClass("active");
-        changeLang(init_lang);
-    }else{
-        $("#lang ." + default_lang).addClass("active").siblings().removeClass("active");
-        changeLang(default_lang);
-    }
-
-
-    // click event on language
-
-    $("#lang > div").click(function () {
-        if ($(this).hasClass("active")) {
-            $(this).parent().toggleClass("show");
-        }else{
-            $(this).addClass("active").siblings().removeClass("active");
-            $(this).parent().removeClass("show");
-            let lang = $(this).data("lang")
-            localStorage.setItem("lang", lang);
-            changeLang(lang)
-        }
-    })
-
-
-    // custom function
-    function changeLang(lang){
-        $(".lang").each(function(){
-            $(this).text($(this).data(lang));
-        });
-        
-        $(".lang-show").each(function(){
-            $(this).hide()
-            if($(this).hasClass(lang)){
-                $(this).show()
-            }
-        });
-
-        $(".lang-mail").each(function(){
-            $(this).attr("href", $(this).data(lang));
-        });
-        
-    }
-
-
-
-
-    /*Wow Animations*/
-    if ($(".wow").length && $(window).outerWidth() >= 567) {
-        let wow = new WOW({
-            boxClass: 'wow',
-            animateClass: 'animated',
-            offset: 0,
-            mobile: false,
-            live: true
-        });
-        wow.init();
-    }
-
-
-
-
-
-
-
-    /* ------- Smooth scroll ------- */
-    $("a.pagescroll").on("click", function (event) {
-        event.preventDefault();
-        let action = $(this.hash).offset().top;
-        $("html,body").animate({
-            scrollTop: action
-        }, 1200);
+    $(".lang-show").each(function () {
+      $(this).hide();
+      if ($(this).hasClass(lang)) {
+        $(this).show();
+      }
     });
 
+    $(".lang-mail").each(function () {
+      $(this).attr("href", $(this).data(lang));
+    });
+  }
 
+  /*Wow Animations*/
+  if ($(".wow").length && $(window).outerWidth() >= 567) {
+    let wow = new WOW({
+      boxClass: "wow",
+      animateClass: "animated",
+      offset: 0,
+      mobile: false,
+      live: true,
+    });
+    wow.init();
+  }
 
+  /* ------- Smooth scroll ------- */
+  $("a.pagescroll").on("click", function (event) {
+    event.preventDefault();
+    let action = $(this.hash).offset().top;
+    $("html,body").animate(
+      {
+        scrollTop: action,
+      },
+      1200
+    );
+  });
 
-
-    // slider
-    $('.owl-carousel').owlCarousel({
-        loop: true,
+  // slider
+  $("#home_slider").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 4,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
         nav: true,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      768: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
         items: 4,
-        margin: 10,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: true
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            768: {
-                items: 3,
-                nav: false
-            },
-            1000: {
-                items: 4,
-                nav: true,
-                loop: false
-            }
-        }
-    })
+        nav: true,
+        loop: false,
+      },
+    },
+  });
 
+  $("#services_slider").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 2,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      768: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 2,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
 
+  // side nav open/close
 
-    $("#nav_opener").click(function () {
-        $("#menu").toggleClass("active");
-        $(this).toggleClass("active")
-        $("body").toggleClass("no_overflow")
-    })
+  $("#nav_opener").click(function (e) {
+    e.preventDefault();
+    $("#menu").toggleClass("active");
+    $(this).toggleClass("active");
+    $("body").toggleClass("no_overflow");
+  });
 
+  // accordion toggle
 
-
-})
+  $(".accordion .toggle").click(function (e) {
+    e.preventDefault();
+    $(this).parent().toggleClass("show");
+    var inner = $(this).next();
+    inner.slideToggle(350, function(){
+        inner.toggleClass("fadingIn")
+    });
+  });
+});
