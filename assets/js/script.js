705f4b6a1ab6d3a49dc4333d40da626f0cf49ee4
atif11jutt <atif11jutt@gmail.com>
2022-06-01 00:06:42 +0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -277,11 +277,11 @@ $(document).ready(function () {
   
   function cookiesPolicyBar(){
       // Check cookie 
-      if ($.cookie('yourCookieName') != "active") $('#cookieAcceptBar').show(); 
+      if ($.cookie('yourCookieName') != "active") $('#cookieAcceptPopup').show(); 
       //Assign cookie on click
       $('#cookieAcceptBarConfirm').on('click',function(){
           $.cookie('yourCookieName', 'active', { expires: 1 }); // cookie will expire in one day
-          $('#cookieAcceptBar').fadeOut();
+          $('#cookieAcceptPopup').hide();
       });
   }
 
